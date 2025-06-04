9c04c5d44d8558ae95f08f25e2268c5442035314
Joseph Mace <charliecat77@gmail.com>
2025-06-04 17:29:03 -0500

    Create Word Frequency Counter.py
    This program Tokenizes the phrase and then counts the uses of each word. The output should be sorted in descending order.

---
+++ Word Frequency Counter.py
@@ -0,0 +1,29 @@
+import nltk
+import collections
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import string
+
+def word_freq(text):
+    #Tokenize text into words
+    tokens =word_tokenize(text)
+    
+    #Noramlize tokens by removing puncuation and switching to lowercase
+    words = [word.lower() for word in tokens if word.isalpha()]
+    
+    #Count word frequency
+    freq = Counter(words)
+    
+    # Sort words by frequency descending
+    sorted_freq = freq.most_common()
+    
+    return sorted_freq
+
+if __name__ == "__main__":
+    n = input("Please enter a phrase: ")
+    
+    frequencies = word_freq(n)
+    
+    for word, count in frequencies:
+        print(f"{word}: {count}")
+    
